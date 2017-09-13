bd71c414fc1c2e1015763e470deff9df9223dbf4
Yuchen Wang <yuchn.w@gmail.com>
2017-09-12 22:52:54 -0400

    Finish etch-a-sketch and it works

--- hw02/sketch.py
+++ hw02/sketch.py
@@ -18,11 +18,11 @@ curses.cbreak()
 screen.addch(y,x,'x')
 
 #Define BTNs
-BTN1="GP0_6"
-BTN2="GP0_4"
+BTN4="GP0_6"
 BTN3="GP0_5"
-BTN4="GP0_3"
-BTN5="RESET"
+BTN2="GP0_4"
+BTN1="GP0_3"
+BTN5="PAUSE"
 
 #Assign GPIO to inputs and outputs
 GPIO.setup(BTN1, GPIO.IN)
@@ -32,9 +32,9 @@ GPIO.setup(BTN4, GPIO.IN)
 GPIO.setup(BTN5, GPIO.IN)
 
 #Map BTNs to related LEDs
-map={BTN1:'a',BTN2:'d',BTN3:'w',BTN4:'s',BTN5:'c'}
+map={BTN1:'d',BTN2:'a',BTN3:'s',BTN4:'w',BTN5:'c'}
 
-#Update LED
+#Update Screen
 def updateSketch(channel):
     global x
     global y
@@ -43,27 +43,44 @@ def updateSketch(channel):
 	if x>0:
 	   x=x-1
 	   screen.addch(y,x,'x')
+	   screen.refresh()
     elif map[channel]=='d':
         if x<width:
 	   x=x+1
            screen.addch(y,x,'x')
+           screen.refresh()
     elif map[channel]=='w':
         if y>0:
 	   y=y-1
 	   screen.addch(y,x,'x')
+           screen.refresh()
     elif map[channel]=='s':
         if y<height:
 	   y=y+1
 	   screen.addch(y,x,'x')
+           screen.refresh()
     elif map[channel]=='c':
 	x=0
 	y=0
 	screen.clear()
+        screen.refresh()
 
+time.sleep(0.1)
 #Detect button pressed
 GPIO.add_event_detect(BTN1,GPIO.BOTH,callback=updateSketch)
 GPIO.add_event_detect(BTN2,GPIO.BOTH,callback=updateSketch)
 GPIO.add_event_detect(BTN3,GPIO.BOTH,callback=updateSketch)
 GPIO.add_event_detect(BTN4,GPIO.BOTH,callback=updateSketch)
 GPIO.add_event_detect(BTN5,GPIO.BOTH,callback=updateSketch)
+
+time.sleep(0.1)
+
+try:
+    while True:
+          time.sleep(100)
+except KeyboardInterrupt:
+    print("Cleaning Up")
+    GPIO.cleanup()
+GPIO.cleanup()
+
 
